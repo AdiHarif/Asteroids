7bf5c7df45cacf5b92a9ff457670c25a9cacc90b
AdiHarif <adi.harif@campus.technion.ac.il>
2020-09-27 12:30:31 +0300

    fixed conflicts with collision merge

--- classes/enemy.py
+++ classes/enemy.py
@@ -19,11 +19,6 @@ class Enemy(Entity):
 		speed_angle = uniform(0, 360)
 		start_vel = uniform(Enemy.MIN_VELOCITY, Enemy.MAX_VELOCITY)
 		self.speed = [start_vel*cos(radians(speed_angle)), start_vel*sin(radians(speed_angle))]
-<<<<<<< HEAD
-	
-	def die(self):
-		return []
-=======
 
 
 
@@ -37,4 +32,3 @@ class Enemy(Entity):
 			for i in range(new_enemies_count):
 				new_enemies.append(Enemy(self.window_size, self.scale/2, self.pos))
 		return new_enemies
->>>>>>> 84db758b6029a18f685f43dc9f47415c7f92a34a
